14da4e70631f74bf30c06c761e6fe9eb9d78e569
santi <sdvillal@gmail.com>
2016-08-08 11:11:23 +0200

    conda environment

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from setuptools import setup, find_packages
 setup(
     name='simone',
     packages=find_packages(),
-    requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'argh', 'openpyxl']
+    requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'argh', 'openpyxl', 'future']
 )
 
