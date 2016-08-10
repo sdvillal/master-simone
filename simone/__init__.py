1c95a4106b729cb72f7a1f6e90def6c6127feb3f
santi <sdvillal@gmail.com>
2016-08-10 12:18:53 +0200

    better organisation of public exports

--- simone/__init__.py
+++ simone/__init__.py
@@ -1,3 +1,8 @@
 # coding=utf-8
 
-from .munging import read_all_data, read_experiments_df, MY_DIR, DATA_DIR, EXPS_NON_STAIRCASE_COLUMNS
+from .munging import (read_all_data,
+                      read_experiments_df,
+                      read_colors_df,
+                      MY_DIR,
+                      DATA_DIR,
+                      EXPS_NON_STAIRCASE_COLUMNS)
